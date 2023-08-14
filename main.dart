5addb0bcddb0b0548e743c69d8106c11d49cc027
InGwonKang <tnqkr3494@naver.com>
2023-08-14 22:35:22 +0900

    qq

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
-String sayHello(String name, int age, [String? country = ""]) {
-  return 'Hello ${name}, You are ${age} from the ${country}';
+String capitalizeName(String? name) {
+  return name?.toUpperCase() ?? "";
 }
+//?? 연산자를 이용하면 왼쪽 값이 null인지 체크해서 null이 아니면 왼쪽 값을 리턴하고 null이면 오른쪽 값을 리턴한다.
 
 void main() {
-  var result = sayHello("sugar", 10);
-  print(result);
+  String? name;
+  name ??= "sugar";
+  name = null;
+  name ??= "js";
+  print(name); // js
 }
+//??= 연산자를 이용하면 변수 안에 값이 null일 때를 체크해서 값을 할당해줄 수 있다.
