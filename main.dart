efe6915eed6ab380fe36fbd8b9a021cccb08a984
InGwonKang <tnqkr3494@naver.com>
2023-08-15 08:53:09 +0900

    extends

--- main.dart
+++ main.dart
@@ -1,28 +1,34 @@
-abstract class Human {
-  void walk();
+class Human {
+  final String name;
+  Human(
+      {required this.name}); //required를 하게 된다면 18행 Player의 super에서 :name을 필요로 하게 된다.
+// 만약 required가 없다면 18행에는 super(name);이 된다.
+  void sayHello() {
+    print("Hi my name is $name");
+  }
 }
 
 enum Team { red, blue }
 
 class Player extends Human {
-  String name;
-  int xp;
-  Team team;
+  final Team team;
 
-  Player(
-      {required this.name, required this.xp, required this.team}); // 중괄호를 넣어줌.
+  Player({
+    required this.team,
+    required String name,
+  }) : super(name: name); //super를 통해 부모 클래스와 상호작용할 수 있게 해줌.
 
+  @override
   void sayHello() {
-    print("Hi my name is $name");
+    super.sayHello(); //Human의 print("Hy my name is $name");을 불러온다.
+    print('and I paly for ${team}');
   }
-
-  void walk() {}
 }
 
 void main() {
-  var nico = Player(name: "nico", xp: 1200, team: Team.red)
-    ..name = "las"
-    ..xp = 120000
-    ..team = Team.blue
-    ..sayHello();
+  var player = Player(
+    team: Team.red,
+    name: 'note',
+  );
+  player.sayHello();
 }
