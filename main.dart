8f8d09ed6b6384e54cd28b16c3db52d238ca5186
InGwonKang <tnqkr3494@naver.com>
2023-08-14 23:58:51 +0900

    cascade&enum&abstract

--- main.dart
+++ main.dart
@@ -1,40 +1,28 @@
-class Player {
+abstract class Human {
+  void walk();
+}
+
+enum Team { red, blue }
+
+class Player extends Human {
   String name;
   int xp;
-  int age;
-  String team;
+  Team team;
 
   Player(
-      {required this.name,
-      required this.xp,
-      required this.age,
-      required this.team}); // 중괄호를 넣어줌.
-
-  Player.createBluePlayer({
-    required String name,
-    required int age,
-  })  : this.age = age,
-        this.name = name,
-        this.team = 'blue',
-        this.xp = 0;
+      {required this.name, required this.xp, required this.team}); // 중괄호를 넣어줌.
 
   void sayHello() {
     print("Hi my name is $name");
   }
+
+  void walk() {}
 }
 
 void main() {
-  var player1 = Player(
-    // 이해하기 쉽도록 parameter이 어떤 뜻인지를 함께 작성할 수 있음.
-    name: "nico",
-    xp: 1500,
-    age: 19,
-    team: "blue",
-  );
-
-  player1.sayHello();
-
-  var player2 = Player.createBluePlayer(name: "kang", age: 19);
-
-  player2.sayHello();
+  var nico = Player(name: "nico", xp: 1200, team: Team.red)
+    ..name = "las"
+    ..xp = 120000
+    ..team = Team.blue
+    ..sayHello();
 }
