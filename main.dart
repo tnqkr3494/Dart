57a895d013ec739e32f6df1d5b6051158d65bb45
InGwonKang <tnqkr3494@naver.com>
2023-08-15 09:40:45 +0900

    mixin

--- main.dart
+++ main.dart
@@ -1,34 +1,23 @@
-class Human {
-  final String name;
-  Human(
-      {required this.name}); //required를 하게 된다면 18행 Player의 super에서 :name을 필요로 하게 된다.
-// 만약 required가 없다면 18행에는 super(name);이 된다.
-  void sayHello() {
-    print("Hi my name is $name");
-  }
+mixin Strong {
+  final double strengthLevel = 1500.99; //mixin : 생성자가 없는 constructor
+}
+
+mixin QuickRunner {
+  void runQuick() => print("run!!!!!!!!!!!");
 }
 
 enum Team { red, blue }
 
-class Player extends Human {
+class Player with Strong, QuickRunner {
   final Team team;
 
   Player({
     required this.team,
-    required String name,
-  }) : super(name: name); //super를 통해 부모 클래스와 상호작용할 수 있게 해줌.
-
-  @override
-  void sayHello() {
-    super.sayHello(); //Human의 print("Hy my name is $name");을 불러온다.
-    print('and I paly for ${team}');
-  }
+  });
 }
 
 void main() {
   var player = Player(
     team: Team.red,
-    name: 'note',
   );
-  player.sayHello();
 }
